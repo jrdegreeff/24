33945394fc9dc62e6b06ac2de7efd5e0a14d85da
jrdegreeff <jrdegreeff@gmail.com>
2019-11-23 04:24:12 -0500

    Write functional algorithm

--- operators.py
+++ operators.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 class Operator(ABC):
     """Abstract class for a generic binary operator."""
 
-    def __str__(self):
+    def __repr__(self):
         """
         Returns a string representation of the operator.
         Should be overriden in subclasses.
@@ -30,7 +30,7 @@ class Operator(ABC):
 class Add(Operator):
     """Addition operator."""
 
-    def __str__(self):
+    def __repr__(self):
         """
         Returns a string representation of the operator.
         """
@@ -52,7 +52,7 @@ class Add(Operator):
 class Subtract(Operator):
     """Subtraction operator."""
 
-    def __str__(self):
+    def __repr__(self):
         """
         Returns a string representation of the operator.
         """
@@ -74,7 +74,7 @@ class Subtract(Operator):
 class Multiply(Operator):
     """Multiplication operator."""
 
-    def __str__(self):
+    def __repr__(self):
         """
         Returns a string representation of the operator.
         """
@@ -96,7 +96,7 @@ class Multiply(Operator):
 class Divide(Operator):
     """Division operator."""
 
-    def __str__(self):
+    def __repr__(self):
         """
         Returns a string representation of the operator.
         """
@@ -121,3 +121,34 @@ class Divide(Operator):
             raise ArithmeticError
         return int(a / b)
 
+
+class Operator_Tree():
+    """Represents a binary tree of operators and operands."""
+
+    def __init__(self, operator, branch1, branch2):
+        """
+        Inializes the root of the tree with an operator and two branches.
+
+        Parameters:
+            operator (Operator) the root operator
+            branch1 (Operator_Tree|int) the left branch
+            branch2 (Operator_Tree|int) the right branch
+        """
+        self.operator = operator
+        self.branch1 = branch1
+        self.branch2 = branch2
+
+    def __repr__(self):
+        """Returns a string representation of the operator tree."""
+        return f"({repr(self.branch1)}{repr(self.operator)}{repr(self.branch2)})"
+
+    def eval(self):
+        """
+        Evaluates the operator tree.
+
+        Returns:
+            int the value of the tree
+        Raises:
+            ArithmeticError if any operation in the tree cannot be applied
+        """
+        return self.operator.apply(self.branch1 if isinstance(self.branch1, int) else self.branch1.eval(), self.branch2 if isinstance(self.branch2, int) else self.branch2.eval())
---
+++ twentyfour.py
@@ -0,0 +1,75 @@
+import itertools as it
+import operators as op
+from pprint import pprint
+import random
+
+
+class Board():
+    """Represtents a collection of cards for the 24 game."""
+
+    def __init__(self, deck=[x for x in range(1, 14)] * 4, size=4, target=24):
+        """
+        Initializes the board with a random list of cards.
+
+        Parameters:
+            deck (list) all the cards which could be used
+            size (int) the number of cards to use
+            target (int) the target number
+        """
+        self.cards = random.sample(list(deck), size)
+        self.target = target
+
+    def solve(self, operators={op.Add(), op.Subtract(), op.Multiply(), op.Divide()}):
+        """
+        Generates a list of solutions for the board if solutions exist.
+
+        Parameters:
+            operators (set) collection of available operators
+        Returns:
+            (list) all solutions for the board
+        Raises:
+            TypeError if any of the passed operators is not an instance
+                      of operators.Operator
+        """
+        for operator in operators:
+            if not isinstance(operator, op.Operator):
+                raise TypeError
+        parses = self.parse(self.cards, operators)
+        pprint(list(filter(lambda p: parses[p] == self.target, parses.keys())))
+
+    def parse(self, operands, operators):
+        """
+        Generates all ways to apply the specified operators on the cards.
+
+        Parameters:
+            operands (list) collection of available operands
+            operators (set) collection of available operators
+        Returns:
+            (dict) mapping of all possible applications of operators on the
+                   operands to their value
+        """
+        def helper(operands, operators):
+            if len(operands) <= 1:
+                return operands
+            parses = []
+            for p in it.permutations(range(len(operands)), 2):
+                remaining = [x for i, x in enumerate(operands) if i not in p]
+                for o in operators:
+                    parses += helper(remaining + [op.Operator_Tree(
+                        o, operands[p[0]], operands[p[1]])], operators)
+            return parses
+
+        result = {}
+        for parse in helper(operands, operators):
+            try:
+                result[parse] = parse.eval()
+            except ArithmeticError:  # Discard any parse with an invalid operation.
+                continue
+            except AttributeError:  # Edge case: singleton operand.
+                result[parse] = parse
+        return result
+
+
+if __name__ == '__main__':
+    b = Board()
+    b.solve()
